a1aef6c2fb80bd4d18d27f029b78b3b94e8bb8a3
edyzbelly <77354889+edyzbelly@users.noreply.github.com>
2021-02-03 13:00:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] count = new int[25];
+
+            int pop = 0;
+            int i;
+
+            for (i = 0; i < count.Length; i++)
+            {
+                count[i] = pop;
+                Console.WriteLine("Element value = " + i + " ");
+            }
+        }
+    }
+}
